1d2c01a56fa2cfd2160a1a3a5623a1c6b661d3f7
Rafly Maulana <mraflymaulana@gmail.com>
2022-03-06 22:23:31 +0700

    fix code style + add new features

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# typedoc-theme-natuna
-Custom Typedoc theme for Natuna (Supports version 0.22+).
-This theme pretty much uses the default theme style and mostly works like a plugin instead of a theme.
+# @natunaorg/typedoc-plugin 
+Custom Typedoc plugin for Natuna (Supports version 0.22+).
 
-**In this theme, you can:**
+**In this plugin, you can:**
 1. Create custom sidebar (customNavigations)
 2. Import markdown (staticMarkdownDocs)
+3. Remove primary sidebar navigation (removePrimaryNavigation)
+4. Remove secondary sidebar navigation (removeSecondaryNavigation)
+5. Replace markdown files content (markdownFilesContentReplacement)
 
-See the example in [here](https://natuna.runes.asia/fivem/). <br/>
-To use it, see the example configuration for this theme in [here](https://github.com/natunaorg/fivem/blob/master/tsconfig.json)
+See the example live result in [here](https://natuna.runes.asia/fivem/). <br/>
+To use it, see the example configuration for this plugin in [here](https://github.com/natunaorg/fivem/blob/master/tsconfig.json)
+
+**Use issues tab in the Github repository to report bugs or request features or if you had any questions.**
+
+I tried my best to keep this plugin as simple as possible. I hope you enjoy this plugin!
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as path from "path";
 import {
     Application,
     JSX,
@@ -19,6 +20,9 @@ enum OptionValue {
     README = "readme",
     STATIC_MARKDOWN_FILES = "staticMarkdownDocs",
     CUSTOM_NAVIGATION = "customNavigations",
+    REMOVE_PRIMARY_NAVIGATION = "removePrimaryNavigation",
+    REMOVE_SECONDARY_NAVIGATION = "removeSecondaryNavigation",
+    MARKDOWN_CONTENT_REPLACEMENT = "markdownFilesContentReplacement",
 }
 
 type StaticMarkdownDocs = {
@@ -34,15 +38,12 @@ type CustomNavigations = {
     }[];
 };
 
+type MarkdownContentReplacement = {
+    content: string;
+    replacement: string;
+};
+
 export const load = (app: Application) => {
-    app.renderer.hooks.on("body.begin", (_) => (
-        <script>
-            <JSX.Raw html="console.log(`[Natuna Typedoc] Generating: ${location.href}`)" />
-        </script>
-    ));
-
-    app.renderer.defineTheme("natuna", CustomTheme);
-
     app.options.addDeclaration({
         type: ParameterType.Mixed,
         name: OptionValue.STATIC_MARKDOWN_FILES,
@@ -56,6 +57,29 @@ export const load = (app: Application) => {
         help: "An Array of custom navigation items.",
         defaultValue: [],
     });
+
+    app.options.addDeclaration({
+        type: ParameterType.Boolean,
+        name: OptionValue.REMOVE_PRIMARY_NAVIGATION,
+        help: "Remove the primary navigation?",
+        defaultValue: false,
+    });
+
+    app.options.addDeclaration({
+        type: ParameterType.Boolean,
+        name: OptionValue.REMOVE_SECONDARY_NAVIGATION,
+        help: "Remove the secondary navigation?",
+        defaultValue: false,
+    });
+
+    app.options.addDeclaration({
+        type: ParameterType.Mixed,
+        name: OptionValue.MARKDOWN_CONTENT_REPLACEMENT,
+        help: "An Array of regex and replacement strings.",
+        defaultValue: [],
+    });
+
+    app.renderer.theme = new CustomTheme(app.renderer);
 };
 
 export class CustomTheme extends DefaultTheme {
@@ -73,6 +97,10 @@ export class CustomTheme extends DefaultTheme {
         if (false == !this.application.options.getValue(OptionValue.README).endsWith("none")) {
             urls.push(new UrlMapping<ContainerReflection>("index.html", project, this.reflectionTemplate));
         } else {
+            if (project.readme) {
+                project.readme = this.replaceMarkdownContent(project.readme);
+            }
+
             urls.push(new UrlMapping<ContainerReflection>("modules.html", project, this.reflectionTemplate));
             urls.push(new UrlMapping("index.html", project, this.indexTemplate));
         }
@@ -94,15 +122,46 @@ export class CustomTheme extends DefaultTheme {
 
         if (markdowns && Array.isArray(markdowns) && markdowns.length > 0) {
             for (const markdown of markdowns) {
+                if (!markdown.pageUrl.startsWith("/")) {
+                    throw new Error(`Markdown page url must start with "/", but got "${markdown.pageUrl}"`);
+                }
+
+                markdown.pageUrl = markdown.pageUrl.substring(1);
+
+                if (!path.isAbsolute(markdown.filePath)) {
+                    markdown.filePath = path.join(process.cwd(), markdown.filePath);
+                }
+
+                if (!fs.existsSync(markdown.filePath)) {
+                    throw new Error(`Could not find markdown template at ${markdown.filePath}`);
+                }
+
+                const rawContent = fs.readFileSync(markdown.filePath, "utf8");
+                const content = this.replaceMarkdownContent(rawContent);
+
                 files.push(
                     new UrlMapping(`${markdown.pageUrl}.html`, project, () => {
-                        return this.getRenderContext().markdownTemplate(markdown.filePath);
+                        return this.getRenderContext().markdownTemplate(content);
                     })
                 );
             }
         }
 
         return files;
+    };
+
+    replaceMarkdownContent = (content: string) => {
+        const replacements = this.application.options.getValue(OptionValue.MARKDOWN_CONTENT_REPLACEMENT) as MarkdownContentReplacement[];
+
+        if (replacements && Array.isArray(replacements) && replacements.length > 0) {
+            for (const replacement of replacements) {
+                while (content.match(replacement.content)) {
+                    content = content.replace(replacement.content, replacement.replacement);
+                }
+            }
+        }
+
+        return content;
     };
 }
 
@@ -112,6 +171,27 @@ export class CustomThemeElement extends DefaultThemeRenderContext {
     }
 
     navigation = (props: PageEvent<Reflection>) => {
+        const removePrimaryNavigation = this.options.getValue(OptionValue.REMOVE_PRIMARY_NAVIGATION);
+        const removeSecondaryNavigation = this.options.getValue(OptionValue.REMOVE_SECONDARY_NAVIGATION);
+
+        return (
+            <>
+                {this.customNavigation(props)}
+                {!removePrimaryNavigation && this.primaryNavigation(props)}
+                {!removeSecondaryNavigation && this.secondaryNavigation(props)}
+            </>
+        );
+    };
+
+    markdownTemplate = (content: string) => {
+        return (
+            <div class="tsd-panel tsd-typography">
+                <JSX.Raw html={this.markdown(content)} />
+            </div>
+        );
+    };
+
+    customNavigation = (props: PageEvent<Reflection>) => {
         const navigations = this.options.getValue(OptionValue.CUSTOM_NAVIGATION) as CustomNavigations[];
 
         if (navigations && !Array.isArray(navigations)) {
@@ -125,43 +205,129 @@ export class CustomThemeElement extends DefaultThemeRenderContext {
         }
 
         return (
-            <>
-                <div class="tsd-navigation primary">
-                    {navigations.map((navigation) => (
-                        <>
-                            <JSX.Raw html={this.markdown(`## ${navigation.title}`)} />
+            <nav class="tsd-navigation primary custom">
+                <ul>
+                    {navigations.map((navigation, index) => (
+                        <li class={`navigation-${index}`}>
+                            <h3 class="current">{navigation.title}</h3>
 
-                            {navigation.links.map((links) => (
-                                <JSX.Raw html={this.markdown(`- [${links.label}](${links.href})`)} />
-                            ))}
-                        </>
+                            <ul>
+                                {navigation.links.map((links) => {
+                                    let link = links.href;
+
+                                    if (!link.startsWith("/")) {
+                                        throw new Error(`Custom navigation link must start with "/", but got "${link}"`);
+                                    }
+
+                                    link = link.substring(1);
+                                    const out = this.options.getValue("out");
+                                    const root = path.relative(path.dirname(props.filename), out);
+                                    const href = path.posix.join(root, link);
+
+                                    return (
+                                        <li>
+                                            <a href={href}>{links.label}</a>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </li>
                     ))}
-                </div>
-
-                {this.secondaryNavigation(props)}
-            </>
+                </ul>
+            </nav>
         );
     };
 
-    markdownTemplate = (location: string) => {
-        if (!fs.existsSync(location)) {
-            throw new Error(`Could not find markdown template at ${location}`);
+    primaryNavigation(props: PageEvent<Reflection>) {
+        const link = (mod: DeclarationReflection) => {
+            const current = this.utils.inPath(mod, props.model);
+            let childNav: JSX.Element | undefined;
+            if (current) {
+                const childModules = mod.children?.filter((m) => m.kindOf(ReflectionKind.SomeModule));
+                if (childModules?.length) {
+                    childNav = <ul>{childModules.map(link)}</ul>;
+                }
+            }
+
+            return (
+                <li class={this.utils.classNames({ current }) + " " + mod.cssClasses}>
+                    <a href={this.urlTo(mod)}>{this.utils.wbr(mod.name)}</a>
+                    {childNav}
+                </li>
+            );
+        };
+
+        const modules = props.model.project.getChildrenByKind(ReflectionKind.SomeModule);
+        const projectLinkName = modules.some((m) => m.kindOf(ReflectionKind.Module)) ? "Modules" : "Exports";
+
+        const [ext, int] = this.utils.partition(modules, (m) => m.flags.isExternal);
+
+        if (ext.length === 0) {
+            return (
+                <nav class="tsd-navigation primary">
+                    <ul>
+                        <li class={this.utils.classNames({ current: props.model.isProject() })}>
+                            <a href={this.urlTo(props.model.project)}>{projectLinkName}</a>
+                        </li>
+                        {int.map(link)}
+                    </ul>
+                </nav>
+            );
         }
 
-        const file = fs.readFileSync(location, "utf8");
-
         return (
-            <div class="tsd-panel tsd-typography">
-                <JSX.Raw html={this.markdown(file)} />
-            </div>
+            <nav class="tsd-navigation primary">
+                <ul>
+                    <li class={this.utils.classNames({ current: props.model.isProject() })}>
+                        <a href={this.urlTo(props.model.project)}>{projectLinkName}</a>
+                    </li>
+                    <li class="label tsd-is-external">
+                        <span>Internals</span>
+                    </li>
+                    {int.map(link)}
+                    <li class="label tsd-is-external">
+                        <span>Externals</span>
+                    </li>
+                    {ext.map(link)}
+                </ul>
+            </nav>
         );
-    };
+    }
 
     secondaryNavigation(props: PageEvent<Reflection>) {
         const children = props.model instanceof ContainerReflection ? props.model.children || [] : [];
 
-        // Remove navigations from the main page (e.g. "Home")
-        if (props.model.isProject()) return null;
+        // Multiple entry points, and on main project page.
+        if (props.model.isProject() && props.model.getChildrenByKind(ReflectionKind.Module).length) {
+            return;
+        }
+
+        // TODO: TypeDoc 0.21 did special things here. If there were more than 40
+        // children of this page's parent, it only displayed this page's children.
+        // Otherwise, it displayed *everything*. For now, only display page children.
+        // It seems weird to do this according to a random hardcoded number. At the very
+        // least this should be added as a configurable flag, but maybe even the whole
+        // behavior should be configurable globally...
+
+        const pageNavigation = (
+            <ul>
+                {children
+                    .filter((child) => !child.kindOf(ReflectionKind.SomeModule))
+                    .map((child) => {
+                        return (
+                            <li class={child.cssClasses}>
+                                <a href={this.urlTo(child)} class="tsd-kind-icon">
+                                    {this.utils.wbr(child.name)}
+                                </a>
+                            </li>
+                        );
+                    })}
+            </ul>
+        );
+
+        if (props.model.kindOf(ReflectionKind.SomeModule | ReflectionKind.Project)) {
+            return <nav class="tsd-navigation secondary menu-sticky">{pageNavigation}</nav>;
+        }
 
         return (
             <nav class="tsd-navigation secondary menu-sticky">
@@ -170,19 +336,7 @@ export class CustomThemeElement extends DefaultThemeRenderContext {
                         <a href={this.urlTo(props.model)} class="tsd-kind-icon">
                             {this.utils.wbr(props.model.name)}
                         </a>
-                        <ul>
-                            {children
-                                .filter((child) => !child.kindOf(ReflectionKind.SomeModule))
-                                .map((child) => {
-                                    return (
-                                        <li class={child.cssClasses}>
-                                            <a href={this.urlTo(child)} class="tsd-kind-icon">
-                                                {this.utils.wbr(child.name)}
-                                            </a>
-                                        </li>
-                                    );
-                                })}
-                        </ul>
+                        {pageNavigation}
                     </li>
                 </ul>
             </nav>
@@ -204,6 +358,37 @@ export class CustomThemeElement extends DefaultThemeRenderContext {
             ret.push(str.slice(i));
 
             return ret;
+        },
+        inPath(thisPage: Reflection, toCheck: Reflection | undefined): boolean {
+            while (toCheck) {
+                if (toCheck.isProject()) return false;
+
+                if (thisPage === toCheck) return true;
+
+                toCheck = toCheck.parent;
+            }
+
+            return false;
+        },
+        classNames(names: Record<string, boolean | null | undefined>) {
+            return Object.entries(names)
+                .filter(([, include]) => include)
+                .map(([key]) => key)
+                .join(" ");
+        },
+        partition<T>(iter: Iterable<T>, predicate: (item: T) => boolean): [T[], T[]] {
+            const left: T[] = [];
+            const right: T[] = [];
+
+            for (const item of iter) {
+                if (predicate(item)) {
+                    left.push(item);
+                } else {
+                    right.push(item);
+                }
+            }
+
+            return [left, right];
         },
     };
 }
